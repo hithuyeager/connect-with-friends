c3493726305a148f08913a921f1ab3e5c34f0caa
hithesh <hitheshrhithu41@gmail.com>
2026-06-07 08:06:55 +0000

    websocket code finished but not yet tested

---
+++ app/services/websocket_services.py
@@ -0,0 +1,41 @@
+from websockets.connection_manager import manager
+from fastapi import WebSocket,WebSocketDisconnect
+from datetime import datetime
+
+from core.security import verify_access_token
+
+async def chat_system(
+    websocket: WebSocket,
+    room_id: str,
+    access_token: str
+):
+    user = verify_access_token(access_token)
+    user_id = user["sub"]
+    await manager.connect(websocket,room_id,user_id)
+    notify_entry = {
+        "type" : "system",
+        "message" : f"{user_id} joined the room",
+        "timestamp" : datetime.utcnow().isoformat()
+    }
+
+    await manager.send_to_room(notify_entry,room_id,websocket)
+
+    try:
+        while True:
+            data = await websocket.receive_json()
+            payload = {
+                "type" : "message",
+                "from" : user_id,
+                "message" : data["message"],
+                "room_id" : room_id,
+                "timestamp" : datetime.utcnow().isoformat()
+            }
+            await manager.send_to_room(payload,room_id,websocket)
+    except WebSocketDisconnect:
+        manager.disconnect(user_id,room_id)
+        notify_exit = {
+            "type":"system",
+            "message" : f"{user_id} has left the room",
+            "timestamp" : datetime.utcnow().isoformat()
+        }
+        await manager.disconnect(user_id,room_id)
--- app/websockets/connection_manager.py
+++ app/websockets/connection_manager.py
@@ -1,15 +1,30 @@
 from fastapi import WebSocket
+from  .auth import verify_user
 
 class ConnectionManager:
     def __init__(self):
-        self.rooms: dict[str,list[WebSocket]]
+        self.rooms: dict[str,dict[str,WebSocket]]
 
     async def connect(
         self,
         websocket: WebSocket,
-        room_id: str,
-        user_id: str):
+        room_id: str
+        ):
         await websocket.accept()
+        user_id = verify_user(websocket)
         if room_id not in self.rooms:
-            self.rooms[room_id] = []
-        self.rooms[room_id].append(websocket)
+            self.rooms[room_id] = {}
+        self.rooms[room_id][user_id] = websocket
+    def disconnect(self,user_id: str,room_id: str):
+        if room_id in self.rooms:
+            self.rooms[room_id].pop(user_id)
+        
+    async def send_to_room(self,data :dict,room_id: str,sender: WebSocket):
+        if room_id not in self.rooms:
+            return
+        for connection in self.rooms[room_id]:
+            if connection == sender:
+                continue
+            await connection.send_json(data)
+
+manager = ConnectionManager()
