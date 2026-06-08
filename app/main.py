db4512ee98bfdc74d5b3363a45a79fb134d65db7
hithesh <hitheshrhithu41@gmail.com>
2026-06-08 08:37:59 +0000

    added global exception handler

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 from contextlib import asynccontextmanager
+from fastapi.responses import JSONResponse
 from starlette.middleware.sessions import SessionMiddleware
 
 from db.connection import connect_to_db
 from api.central_api import router
 from config import settings
+from core.errors import UsersErrors
+from schemas.responses import APIResponse
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -17,3 +20,13 @@ app = FastAPI(lifespan=lifespan)
 app.add_middleware(SessionMiddleware,secret_key=settings.google_secret_key)
 
 app.include_router(router)
+ 
+@app.exception_handler(UsersErrors)
+async def global_exception_handler(request: Request,exc: UsersErrors):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content=APIResponse(
+            message="error",
+            data = exc.message
+        )
+    )
