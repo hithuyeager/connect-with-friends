476d62fe6a42bceb5e3def2a48212c7a7f798424
hithesh <hitheshrhithu41@gmail.com>
2026-06-14 09:36:26 +0000

    created room for websockets

--- app/services/websocket_services.py
+++ app/services/websocket_services.py
@@ -1,8 +1,14 @@
 from ws.connection_manager import manager
 from fastapi import WebSocket,WebSocketDisconnect
 from datetime import datetime
+from hashlib import sha256
 
 from core.security import verify_access_token
+
+def generate_room_id(user1_id: str, user2_id: str):
+    sorted_ids = sorted([user1_id,user2_id])
+    combined = f"{sorted_ids[0]:{sorted_ids[1]}}"
+    return sha256(combined.encode()).hexdigest()[:16]
 
 async def chat_system(
     websocket: WebSocket,
@@ -33,8 +39,3 @@ async def chat_system(
             await manager.send_to_room(payload,room_id,user_id)
     except WebSocketDisconnect:
         manager.disconnect(user_id,room_id)
-        notify_exit = {
-            "type":"system",
-            "message" : f"{user_id} has left the room",
-            "timestamp" : datetime.utcnow().isoformat()
-        }
